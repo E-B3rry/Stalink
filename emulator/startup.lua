106e07c5f25316677adc7b738ddb2cb138fe192a
E-B3rry <julot135@outlook.com>
2022-02-12 00:38:11 +0100

    Nightly commit Progressing on redCom, starting to work on the communication's protocols. Also added and configured a ComputerCraft emulator to help in the development of Stalink.

---
+++ emulator/startup.lua
@@ -0,0 +1,24 @@
+--- Emulator startup file (Automatically updated via artifact at every emulator start)
+-- Only triggers if the instance is run in the standalone emulator.
+if ccemux then
+    local sides = {"left", "right", "front", "back", "top", "bottom"}
+    local id = 0
+
+    if type(os.getComputerID()) == "number" then
+        id = os.getComputerID()
+    else
+        id = math.random(1, 4)
+    end
+
+    -- Attach a fake modem to a random side
+    ccemux.attach(sides[math.random(1, #sides)], "wireless_modem", {
+        range = 64,
+        interdimensional = false,
+        world = "main",
+        posX = id * math.random(4, 6),
+        posY = id * math.random(4, 6),
+        posZ = 110,
+    })
+
+    print("[CCEMUX] Attached a fake modem to a random side")
+end
