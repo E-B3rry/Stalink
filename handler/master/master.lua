f4778898a70c50482c3c84cf081914ff6fa1e4ff
Alpa <alpaga.ytb@gmail.com>
2024-10-30 02:11:50 +0100

    g rangé ma chenbre

--- handler/master/master.lua
+++ handler/master/master.lua
@@ -14,38 +14,51 @@ os.loadAPI(StalinkInstallationPath .. "turtle/quarry_mining/quarry_mining.lua")
 os.loadAPI(StalinkInstallationPath .. "utilities/requestUtils.lua")
 os.loadAPI(StalinkInstallationPath .. "redcom/redcom.lua")
 
-
 --getmetatable('').__index = function(str,i) return string.sub(str,i,i) end
 
-function writeF(txt)
-  --print(tostring(txt))
-  peripheral.call("left", "write", (tostring(txt).."\n"))
-end
-
-
-function Ask(RequestObj, rc)
-  if RequestObj == "InitQuarry" then
-    --redcom.lua.open(rc)
-    redcom.sendRaw(10, os.getComputerID(), {["x"] = 10, ["y"] = 10, ["z"] = 10}, nil)
+--this is dumb, but it's the only way for me to print on the screen on multiple lines
+function WriteF(txt)
+	--print(tostring(txt))
+	local screen = peripheral.find("monitor")
+  if screen then
+    local txtstr = tostring(txt)
+    local w, h = screen.getSize()
+    local x, y = screen.getCursorPos()
+    if string.len(txtstr) > w then
+      screen.write(string.sub(txtstr, 1, w))
+      screen.setCursosPos(1, y+1)
+      y = y + 1
+      screen.write(string.sub(txtstr, w, 10))
+    else
+	    screen.write(txtstr)
+    end
+    screen.setCursorPos(1, y+1)
   end
 end
 
+function Ask(RequestObj, rc)
+	if RequestObj == "InitQuarry" then
+		--redcom.lua.open(rc)
+		redcom.sendRaw(10, os.getComputerID(), { ["x"] = 10, ["y"] = 10, ["z"] = 10 }, nil)
+	end
+end
 
 -- Master Stalin --
 redcom.open(10)
-
+local screen = peripheral.find("monitor")
+screen.clear()
 while true do
-  c, rc, data, d = redcom.receiveRaw(true)
-  writeF(data)
-  Ask(data, rc)
+	c, rc, data, d = redcom.receiveRaw(true) -- Receive data from somewhere
+	WriteF(data) -- Writes the data to the screen on the left
+	Ask(data, rc)
+	--writeF(0)
 end
-
 
 -- Request System --
 
-  --{id,msg (ex : QzrF0100 or SzrE1720 or QcrF0950 or QfrE0211)}
-  --msg encryption// first char = first letter of the job in MAJ. thene zr = zone request // cr = chest request // fr = fuel request
-  --E = empty inventory / F = full and M = space left in the inventory
-  --Fuel level in 4 digits
+--{id,msg (ex : QzrF0100 or SzrE1720 or QcrF0950 or QfrE0211)}
+--msg encryption// first char = first letter of the job in MAJ. thene zr = zone request // cr = chest request // fr = fuel request
+--E = empty inventory / F = full and M = space left in the inventory
+--Fuel level in 4 digits
 
-  --Return code : {x,y,z(nil if no need), command}
+--Return code : {x,y,z(nil if no sneed), command}
--- utilities/utils.lua
+++ utilities/utils.lua
@@ -3,7 +3,37 @@ function convertStringTo32Bits(string)
   return 0
 end
 
+function hexcharToInt(c)
+  if c == "0" then
+    return 0
+  end
+  if c == "1" then
+    return 1
+  end
+  if c == "2" then
+    return 2
+  end
+  if c == "3" then
+    return 3
+  end
+  if c == "4" then
+    return 4
+  end
+  if c == "5" then
+    return 5
+  end
+end
+
 function convertHexStringToBytesArray(str)
-  print("TO IMPLEMENT")
+  bts = {}
+  for i = 1, #str do
+    local c = str:sub(i,i)
+    local idx = math.floor(i/2)
+    if i%1 == 1 then
+      bts[idx] = hexcharToInt(c)
+    else
+      bts[idx] = bts[idx] + hexcharToInt(c) * 16
+    end
+  end
   return {0}
 end
